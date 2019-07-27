9fc24790d153781c8b03b944e17733e8a0ff95d1
nick bouldien <nick.bouldien@gmail.com>
2019-07-27 16:12:56 -0500

    updates to the about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,21 @@
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>
+  This site is built using
+  <a href="https://svelte.dev/" target="_blank">Svelte</a>
+  and the Svelte framework
+  <a href="https://sapper.svelte.dev/" target="_blank">Sapper</a>
+  .
+  <br />
+  The github repo for this project can be found
+  <a href="https://github.com/nickbouldien/can-i-drink-yet" target="_blank">
+    here
+  </a>
+  .
+  <br />
+  Feel free to contribute and add any locations.
+</p>
