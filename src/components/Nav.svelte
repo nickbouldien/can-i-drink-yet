11a99af6640ecaf32d45017deceeefea41fcce2d
nick bouldien <nick.bouldien@gmail.com>
2019-07-27 15:45:29 -0500

    change color of selected link 'bottom border':

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -36,7 +36,7 @@
     content: "";
     width: calc(100% - 1em);
     height: 2px;
-    background-color: rgb(255, 62, 0);
+    background-color: #022144;
     display: block;
     bottom: -1px;
   }
